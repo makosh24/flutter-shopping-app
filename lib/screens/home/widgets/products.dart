f62171c31f51b8fbe46d35600ff0c1b083c72c15
Madi Yedenbay <makosh24@mail.ru>
2022-04-05 16:12:35 +0600

    my option

--- lib/screens/home/widgets/products.dart
+++ lib/screens/home/widgets/products.dart
@@ -7,14 +7,16 @@ import 'package:get/get.dart';
 class ProductsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Obx(()=>GridView.count(
+    return Obx(() => GridView.count(
         crossAxisCount: 2,
         childAspectRatio: .63,
         padding: const EdgeInsets.all(10),
         mainAxisSpacing: 4.0,
         crossAxisSpacing: 10,
         children: producsController.products.map((ProductModel product) {
-          return SingleProductWidget(product: product,);
+          return SingleProductWidget(
+            product: product,
+          );
         }).toList()));
   }
 }
